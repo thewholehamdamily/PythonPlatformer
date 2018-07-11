fde88a5a5734ef136facfa51155bc72ef1eabf91
Cameron Furuseth <furusethc@gmail.com>
2018-07-11 11:30:23 -0400

    Player class file

---
+++ player.py
@@ -0,0 +1,58 @@
+import pygame
+HEIGHT = 600
+WIDTH = 800
+RED = (255,0,0)
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self):
+		super().__init__()
+		width = 40
+		height = 60
+		self.image = pygame.Surface([width,height])
+		self.image.fill(RED)
+		self.rect = self.image.get_rect()
+
+		self.change_x = 0
+		self.change_y = 0
+		self.level = None
+	def update(self):
+		self.get_gravity()
+		self.rect.x += self.change_x
+
+		block_hit_list = pygame.sprite.spritecollide(self,self.level.platform_list)
+		for block in block_hit_list:
+			if self.change_x > 0:
+				self.rect.right = block.rect.left
+			elif self.change_x < 0:
+				self.rect.left = block.rect.right
+		self.rect.y += self.change_y
+		block_hit_list = pygame.sprite.spritecollide(self,self.level.platform_list)
+		for block in block_hit_list:
+			if self.change_y > 0:
+				self.rect.bottom = block.rect.top
+			elif self.change_y < 0:
+				self.rect.top = block.rect.bottom
+			self.change_y = 0
+
+	def get_gravity(self):
+		if self.change_y == 0:
+			self.change_y = 1
+		else:
+			self.change_y += .35
+		if self.rect.y >= HEIGHT - self.rect.height and self.change_y >= 0:
+			self.change_y = 0
+			self.rect.y = HEIGHT - self.rect.height
+
+	def jump(self):
+		self.rect.y += 2
+		platform_hit_list = pygame.sprite.spritecollide(self,self.level.platform_l)
+		self.rect.y -= 2
+		if len(platform_hit_list) > 0 or self.rect.bottom >= HEIGHT:
+			self.change_y = -10
+
+	def left(self):
+		self.change_x = -6
+	def right(self):
+		self.change_x = 6
+	def stop(self):
+		self.change_x = 0
