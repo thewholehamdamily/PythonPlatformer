a989c132243f2e07ec4f05ddf6b529711be18c2a
duplessis96 <ddd16@my.fsu.edu>
2018-07-31 18:06:36 -0400

    Add files via upload

---
+++ Boss_Projectiles.py
@@ -0,0 +1,26 @@
+import pygame 
+from settings import *
+
+RED = (255, 0, 0) 
+
+class Boss_Projectiles(pygame.sprite.Sprite): 
+	
+	def __init__(self, d, x, y): 
+
+		super().__init__()
+		width = 20			        #Made the Width and Height larger for the boss
+		height = 20
+		self.image.Surface[width,height]) 
+		self.image.fill(RED) 
+		self.rect = self.image.get_rect() 
+		self. = d
+
+	def update(self): 
+		
+		self.rect.y += sin(self.direct) * 3 
+		self.rect.y += cos(self.direct) * 3 
+
+		if self.rect.x < -16 or self.rect.x > 800 or self.rect.y < 0 or self.rect.y > 600:
+		
+			self.kill()
+
