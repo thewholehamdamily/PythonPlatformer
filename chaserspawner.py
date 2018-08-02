b65d8ea106eff478b2d286d82a2072d47c3b9109
ReddenMoney <36646579+ReddenMoney@users.noreply.github.com>
2018-08-01 21:25:51 -0400

    Chaser spawner fix

--- chaserspawner.py
+++ chaserspawner.py
@@ -6,16 +6,19 @@ class ChaserSpawner(pygame.sprite.Sprite):
 		width = 32
 		height = 32
 		self.image = pygame.Surface([width,height])
-		draw.rect(self.image,(255,255,255),(x,y,width,height),1)
+		pygame.draw.rect(self.image,(255,255,255),(x,y,width,height),3)
 		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
 		self.fireRate = 300
 		self.alarm = 0
 		self.id = 3
+		self.vulnerable = 2
 
 	def update(self):
 		self.alarm += 1
+	def attack(self):
+		pass
 		
 
 	
